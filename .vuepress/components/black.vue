271077277fdd8a7e2389f4a9e9121376c55cc3e4
Yesterday17 <t@yesterday17.cn>
2019-05-31 16:05:26 +0800

    refactor: use vuepress

---
+++ .vuepress/components/black.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="heimu" :title="title">
+    <slot></slot>
+  </span>
+</template>
+
+<style>
+.heimu,
+.heimu a,
+a .heimu {
+  background-color: #252525;
+  color: #252525;
+  text-shadow: none;
+}
+
+.heimu p {
+  display: inline;
+}
+
+.heimu:hover,
+.heimu:active {
+  transition: color 0.13s linear;
+  color: #fff;
+}
+
+.heimu:hover a {
+  transition: color 0.13s linear;
+  color: #add8e6;
+}
+
+.heimu .icon.outbound {
+  display: none;
+}
+</style>
+
+
+<script>
+export default {
+  props: ["title"]
+};
+</script>
---
+++ .vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  title: "Kyandy",
+  description: "歩き出すんだ、自分の軌跡になるから、止まらない。",
+  dest: "./_book/",
+  serviceWorker: true,
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    repo: "Yesterday17/Kyandy",
+    repoLabel: "GitHub",
+    editLinks: true,
+    editLinkText: "来帮忙吗？",
+    nav: [
+      { text: "Blog", link: "https://www.yesterday17.cn/" },
+      {
+        text: "Melody Flag",
+        link: "https://mmf.moe/"
+      }
+    ],
+    sidebar: {
+      "/": [
+        {
+          title: "前言",
+          children: [
+            ["/preface/", "写在前面：这是什么？"],
+            ["/preface/kyandy", "0.1 序：水中キャンデイ"],
+            ["/preface/video", "0.2 【数据删除】"]
+          ]
+        },
+        {
+          title: "1. 在一切开始之前",
+          children: [
+            // ["/chapter-01/", "在一切开始之前"],
+            ["/chapter-01/prepare", "1.1 准备工作"],
+            ["/chapter-01/structure", "1.2 基本架构"]
+          ]
+        }
+      ]
+    },
+    displayAllHeaders: true,
+    lastUpdated: "最后更新"
+  },
+  evergreen: true
+};
