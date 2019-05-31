0d2cfa484bfef458ab0cf40cd5f6d27c6fb63b8f
Yesterday17 <t@yesterday17.cn>
2019-06-01 00:54:08 +0800

    feat: source part

---
+++ source/config.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {
+    title: "ForestryMC",
+    children: [["", "关于源码"], ["forestry", "Forestry.java"]]
+  }
+];
---
+++ source/forestry.md
@@ -0,0 +1,9 @@
+# Forestry.java
+
+## 源码
+
+<<< @/ForestryMC/src/main/java/forestry/Forestry.java
+
+## 详解
+
+TODO
