1752feeef2bd8353b37d92e921359d0487b3f990
Yesterday17 <t@yesterday17.cn>
2019-05-31 23:13:08 +0800

    feat(ch01): yakusoku

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -32,11 +32,15 @@ module.exports = {
         {
           title: "1. 在一切开始之前",
           children: [
-            // ["/chapter-01/", "在一切开始之前"],
             ["/chapter-01/prepare", "1.1 准备工作"],
-            ["/chapter-01/structure", "1.2 基本架构"]
+            ["/chapter-01/structure", "1.2 基本架构"],
+            ["/chapter-01/yakusoku", "1.3 一些约定"]
           ]
         }
+        // {
+        //   title: "2. core 模块",
+        //   children: [["", "2.1 简介"]]
+        // }
       ]
     },
     displayAllHeaders: true,
---
+++ chapter-01/yakusoku.md
@@ -0,0 +1,13 @@
+# 一些约定
+
+这里列出一些后文中会用到的东西，当你后文有看不懂的内容时，在发 [issue][kyandy-issue] 提醒我好好说话<black title="草">会说话就多说点</black>之前不妨来这里找找。
+
+[kyandy-issue]: https://github.com/Yesterday17/Kyandy/issues
+
+## 模块与插件
+
+模块对应林业里的 `Module`，而插件对应的则是 `Plugin`。
+
+值得注意的是，虽然在[Module？Plugin？][module-or-plugin]里我说了 `Plugin` 的个体本质上也是 `Module`，但在后文里，除非明文指出，否则其代表的内容都不相同。
+
+[module-or-plugin]: structure.html#module%EF%BC%9Fplugin%EF%BC%9F
