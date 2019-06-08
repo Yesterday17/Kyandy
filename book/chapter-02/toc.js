3f42c292b5f067f1afa1dfc551cc55b894be6656
Yesterday17 <t@yesterday17.cn>
2019-06-08 17:42:55 +0800

    refactor(ch02); rename to order files

--- book/chapter-02/toc.js
+++ book/chapter-02/toc.js
@@ -1,9 +1,9 @@
 module.exports = {
   title: "2. 模块的加载过程",
   children: [
-    ["chapter-02/module-and-container", "2.1 模块与 Container"],
-    ["chapter-02/forestry-modules", "2.2 ForestryModules"],
-    ["chapter-02/blank-forestry-module", "2.3 BlankForestryModule"],
-    ["chapter-02/module-manager", "2.4 ModuleManager"]
+    ["chapter-02/01-module-and-container", "2.1 模块与 Container"],
+    ["chapter-02/02-forestry-modules", "2.2 ForestryModules"],
+    ["chapter-02/03-blank-forestry-module", "2.3 BlankForestryModule"],
+    ["chapter-02/04-module-manager", "2.4 ModuleManager"]
   ]
 };
