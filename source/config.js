95febc31f24a043cf0ae8621982a76317468fd59
Yesterday17 <t@yesterday17.cn>
2019-08-26 18:51:42 +0800

    feat(source): readme & licenses

--- source/config.js
+++ source/config.js
@@ -1,34 +1,32 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const base = "./source/";
-
-const children = [["", "关于源码"]];
+const base = './source/'
 
 function IsDirectory(dir, name) {
-  return fs.statSync(path.resolve(base, dir, name)).isDirectory();
+  return fs.statSync(path.resolve(base, dir, name)).isDirectory()
 }
 
-function IterateDir(dir = "") {
-  const files = fs.readdirSync(path.resolve(base, dir));
-  const ch = [];
+function IterateDir(dir = '') {
+  const files = fs.readdirSync(path.resolve(base, dir))
+  const ch = []
   files.forEach(file => {
     if (IsDirectory(dir, file)) {
-      ch.push(IterateDir(dir + file + "/"));
-    } else if (file === "Readme.md") {
-      return;
+      ch.push(IterateDir(dir + file + '/'))
+    } else if (file === 'Readme.md') {
+      return
       // ch.unshift(dir, "About");
-    } else if (path.extname(file) === ".md") {
+    } else if (path.extname(file) === '.md') {
       ch.push([
-        dir + path.basename(file, ".md"),
-        path.basename(file, ".md") + ".java"
-      ]);
+        dir + path.basename(file, '.md'),
+        path.basename(file, '.md') + '.java'
+      ])
     }
-  });
+  })
   return {
-    title: dir === "" ? "ForestryMC" : dir,
+    title: dir === '' ? 'ForestryMC' : dir,
     children: ch
-  };
+  }
 }
 
-module.exports = [IterateDir()];
+module.exports = [['', '关于源码'], IterateDir()]
